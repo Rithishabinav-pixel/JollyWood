5107ba40ed0d1dc04d60d6ea7e1359e05b6cd1ae
Rithish Abinav <rithish.pixel@gmail.com>
2026-09-15 14:37:22 +0530

    Add

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -42,7 +42,7 @@ export default function RootLayout({ children }) {
     <SmoothScroll/>
     {/* <AOSInit/> */}
 <HeaderWrapper />
-{/* <FloatingTicket/> */}
+<FloatingTicket/>
         <main>
     <FloatingAnnouncement/>
    <DesktopSidebar />
