1efb275ee949c847478b0f8e8a150abc183b74df
Rithish Abinav <rithish.pixel@gmail.com>
2026-09-16 12:05:40 +0530

    Improved pagespeed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="stylesheet" href="https://use.typekit.net/twz0jrm.css"/>
-        <link rel="preload" as="image" href="/assets/images/banner-poster.webp" fetchPriority="high"/>
+       <link rel="preconnect" href="https://use.typekit.net" />
+       <link rel="preload" as="image" href="/assets/images/banner-poster.webp" fetchPriority="high"/>
+       <link rel="stylesheet" href="https://use.typekit.net/twz0jrm.css"/>
       </head>
 
       <body suppressHydrationWarning>{children}</body>
